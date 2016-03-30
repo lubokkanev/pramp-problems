82ca32bf4a505640005a71e6ce7ce411b48c8b8a
lubokkanev <LuboKKanev@gmail.com>
2016-03-30 11:43:35 +0300

    Add the solution to finding an element in a sorted but shifted array

--- src/com/lubokkanev/pramp/Main.java
+++ src/com/lubokkanev/pramp/Main.java
@@ -1,7 +1,9 @@
 package com.lubokkanev.pramp;
 
+import com.lubokkanev.pramp.problems.P00ShiftedArrayFinder;
+
 public class Main {
     public static void main(String[] args) {
-        
+        P00ShiftedArrayFinder.test();
     }
 }
