e1605dc1c8a06adafb0c055d6bb59bb725f6635a
LuboKKanev <LuboKKanev@gmail.com>
2016-04-24 13:35:26 +0300

    Refactor equal value index finder tests
    Make expected and actual result variables.

--- src/test/java/com/lubokkanev/pramp/problems/P04EqualValueIndexFinderTest.java
+++ src/test/java/com/lubokkanev/pramp/problems/P04EqualValueIndexFinderTest.java
@@ -12,16 +12,6 @@ public class P04EqualValueIndexFinderTest {
 
     private P04EqualValueIndexFinder classUnderTest;
 
-    private <T> boolean inSet(T result, HashSet<T> rightAnswers) {
-        for (T answer : rightAnswers) {
-            if (answer.equals(result)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     @Before
     public void setUp() {
         classUnderTest = new P04EqualValueIndexFinder(null);
@@ -31,28 +21,36 @@ public class P04EqualValueIndexFinderTest {
     public void testFindIndex_oneElement_found() {
         classUnderTest.setArray(new ArrayList<>(Collections.singletonList(0)));
 
-        assertEquals(0, classUnderTest.findIndex());
+        Integer expectedResult = 0;
+        Integer actualResult = classUnderTest.findIndex();
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testFindIndex_oneElement_notFound() {
         classUnderTest.setArray(new ArrayList<>(Collections.singletonList(1)));
 
-        assertEquals(-1, classUnderTest.findIndex());
+        Integer expectedResult = -1;
+        Integer actualResult = classUnderTest.findIndex();
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testFindIndex_normal_notFound() {
         classUnderTest.setArray(new ArrayList<>(Arrays.asList(5, 6, 7)));
 
-        assertEquals(-1, classUnderTest.findIndex());
+        Integer expectedResult = -1;
+        Integer actualResult = classUnderTest.findIndex();
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testFindIndex_normal_found() {
         classUnderTest.setArray(new ArrayList<>(Arrays.asList(-1, 0, 1, 2, 4, 5)));
 
-        assertTrue(inSet(classUnderTest.findIndex(), new HashSet<>(Arrays.asList(4,5))));
+        Set<Integer> expectedResult = new HashSet<>(Arrays.asList(4,5));
+        Integer actualResult = classUnderTest.findIndex();
+        assertTrue(expectedResult.contains(actualResult));
     }
 
     @Test
@@ -60,7 +58,9 @@ public class P04EqualValueIndexFinderTest {
         classUnderTest.setArray(new ArrayList<>(Arrays.asList(-100, -99, -98, -97, -90, -80, -22,
                 5, 7, 9, 11)));
 
-        assertEquals(9, classUnderTest.findIndex());
+        Integer expectedResult = 9;
+        Integer actualResult = classUnderTest.findIndex();
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
@@ -68,6 +68,8 @@ public class P04EqualValueIndexFinderTest {
         classUnderTest.setArray(new ArrayList<>(Arrays.asList(-100, -99, -98, -97, -90, -80, -22,
                 5, 7, 10, 11)));
 
-        assertEquals(-1, classUnderTest.findIndex());
+        Integer expectedResult = -1;
+        Integer actualResult = classUnderTest.findIndex();
+        assertEquals(expectedResult, actualResult);
     }
 }
