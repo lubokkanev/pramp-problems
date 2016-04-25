82ac40516a6ed665fbf56d210225cdbee2efa987
LuboKKanev <LuboKKanev@gmail.com>
2016-04-26 01:11:01 +0300

    Remove redundant test method
    Remove the list print method from the word count engine test class,
    because it's no longer needed.

--- src/test/java/com/lubokkanev/pramp/problems/P05WordCountEngineTest.java
+++ src/test/java/com/lubokkanev/pramp/problems/P05WordCountEngineTest.java
@@ -21,6 +21,7 @@ public class P05WordCountEngineTest {
 
     @Test
     public void testCountWords_normal() throws IOException {
+        // TODO: move the file to test resources
         file = new File("test.txt");
         Writer writer = new FileWriter(file);
         writer.write("one two three \ntwo three \nthree");
@@ -32,15 +33,6 @@ public class P05WordCountEngineTest {
         List<P05WordCountEngine.WordCount> expectedResult = new ArrayList<>(Arrays.asList(new P05WordCountEngine.WordCount("three", 3), new P05WordCountEngine.WordCount("two", 2), new P05WordCountEngine.WordCount("one", 1)));
         List<P05WordCountEngine.WordCount> actualResult = classUnderTest.countWords();
 
-        printList(expectedResult);
-        printList(actualResult);
-
         assertEquals(expectedResult, actualResult);
-    }
-
-    private void printList(List<P05WordCountEngine.WordCount> list) {
-        for (P05WordCountEngine.WordCount wordCount : list) {
-            System.out.println(wordCount.word + ": " + wordCount.count);
-        }
     }
 }
