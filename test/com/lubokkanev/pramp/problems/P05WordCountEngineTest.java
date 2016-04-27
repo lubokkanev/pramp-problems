213ad9bdabb15b4e24d7ffe7077cadf752c4bb5a
LuboKKanev <LuboKKanev@gmail.com>
2016-04-27 21:57:26 +0300

    Add a test to the word counting engine

--- test/com/lubokkanev/pramp/problems/P05WordCountEngineTest.java
+++ test/com/lubokkanev/pramp/problems/P05WordCountEngineTest.java
@@ -1,22 +1,26 @@
 package com.lubokkanev.pramp.problems;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class P05WordCountEngineTest {
     private P05WordCountEngine classUnderTest;
-    private File file = new File("test/resources/test.txt");
+    private final File file = new File("test/resources/test.txt");
+    private FileWriter writer;
 
     @Before
     public void setUp() throws IOException {
-        classUnderTest = new P05WordCountEngine(null);
+        classUnderTest = new P05WordCountEngine(file);
+        writer = new FileWriter(file);
     }
 
     @Test
@@ -26,18 +30,31 @@ public class P05WordCountEngineTest {
         String three = "three";
         writeToFile(one + " " + two + " " + three + " \n" + two + " " + three + " \n" + three);
 
-        classUnderTest.setFile(file);
-
         List<P05WordCountEngine.WordCount> expectedResult = new ArrayList<>(Arrays.asList(new P05WordCountEngine.WordCount(three, 3), new P05WordCountEngine.WordCount(two, 2), new P05WordCountEngine.WordCount(one, 1)));
         List<P05WordCountEngine.WordCount> actualResult = classUnderTest.countWords();
 
         assertEquals(expectedResult, actualResult);
     }
 
+    @Test
+    public void testCountWords_one() throws IOException {
+        String three = "three";
+        writeToFile(three + " " + three + "\n" + three);
+
+
+        List<P05WordCountEngine.WordCount> expectedResult = new ArrayList<>(Collections.singletonList(new P05WordCountEngine.WordCount(three, 3)));
+        List<P05WordCountEngine.WordCount> actualResult = classUnderTest.countWords();
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        writer.close();
+    }
+
     private void writeToFile(String content) throws IOException {
-        Writer writer = new FileWriter(file);
         writer.write(content);
         writer.flush();
-        writer.close();
     }
 }
